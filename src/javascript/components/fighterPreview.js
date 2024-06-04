76ac981dd069b1de3cac2455af312ae1d2020e09
Ihor Artomov <igora2406@gmail.com>
2024-06-04 14:02:21 +0300

    + function createFighterPreview without styles

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -24,6 +12,27 @@ export function createFighterImage(fighter) {
         className: 'fighter-preview___img',
         attributes
     });
-
     return imgElement;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+    // todo: show fighter info (image, name, health, etc.)
+    if (fighter) {
+        const { name, health, attack, defense, source } = fighter;
+        const infoElement = createElement({
+            tagName: 'p',
+            className: ''
+        });
+        const imgElement = createFighterImage({ name, source });
+        infoElement.innerHTML = `health: ${health} attack: ${attack} defense: ${defense}`;
+        fighterElement.appendChild(infoElement);
+        fighterElement.appendChild(imgElement);
+    }
+
+    return fighterElement;
 }
